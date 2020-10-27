42938e3b9e00b8768c7a62450fcdb97743e3cbc6
Mcheyo <mcheyo.mc@gmail.com>
2020-10-27 10:07:41 -0400

    got shuffle working

--- src/components/questionCard.js
+++ src/components/questionCard.js
@@ -1,26 +1,27 @@
 import React from 'react'
+var shuffle = require('shuffle-array')
+
 
 const questionCard = (props) =>{ 
     const {correct, incorrect, question} = props.questionData
    let  answers = [...incorrect,correct]
 
-   function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+//    function shuffleArray(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         const j = Math.floor(Math.random() * (i + 1));
+//         [array[i], array[j]] = [array[j], array[i]];
+//     }
 
 
-    return array
-}
+
     return( 
         <div>
             <h4>{question}</h4>
             
 
-            {shuffleArray(answers).map(answer =>{
+            {shuffle(answers).map(answer =>{
                 
-               return  <p onClick={(e) => console.log(e.target.innerText)}>
+               return  <p onClick={(e) => e.target.innerText == correct? console.log("goodjob"): console.log("try again")}>
                     {answer}
                 </p>
             })}
--- src/containers/questionContainer.js
+++ src/containers/questionContainer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import QuestionCard from '../components/questionCard'
+ var shuffle = require('shuffle-array')
+
 
 export default class questionContainer extends Component{ 
   state ={ 
@@ -10,12 +12,31 @@ export default class questionContainer extends Component{
     componentDidMount(){ 
         fetch("http://localhost:3000/data")
         .then(res => res.json())
-        .then(data => this.setState({ 
-          questionsArray: data}))
-      }
+        .then(data => this.setState({
+            questionsArray: shuffle(data)
+        }))
+       }
+      
+
+
+   
+    
+    // startGame = () =>{ 
+    //     let questions = [...this.state.questionsArray]
+    //    this.setState({ 
+    //        questionsArray: shuffle(questions)
+    //    })
+       
+       
+    // }
+
+
     render(){ 
         return( 
             <div>
+                <button onClick={() =>this.startGame() }>
+                    Play! 
+                </button>
               {this.state.questionsArray.map(question => <QuestionCard questionData={question}/>)}
             </div>
         )
